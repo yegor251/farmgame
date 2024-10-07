cfc3cbd332c78f8a06675e20d6224658cb2dce73
yegor251 <eagorov.yegor25@gmail.com>
2024-10-08 01:43:11 +0300

    all items img + _front

--- client/resources.js
+++ client/resources.js
@@ -9,7 +9,7 @@ class Resources {
     this.names = {};
     this.buildingNames = {}
     this.names.buildings = [];
-    this.buildingNames.bakery = ["bakery", 'crusher', 'dairy', 'juicer', 'loom', 'oven', 'popcorn_maker', 'sugar_factory']
+    this.buildingNames.bakery = ["bakery", 'crusher', 'dairy', 'sugar_factory', 'popcorn_maker', 'grill', 'oven', 'loom', 'juicer']
     this.buildingNames.garden = ["garden"]
     this.buildingNames.animalPen = ['coop', 'cowshed', 'pigsty', 'sheepfold']
     this.buildingNames.bush = ['cranberry', 'apple_tree', 'raspberry', 'cherry', 'blackberry', 'strawberry']
@@ -20,11 +20,15 @@ class Resources {
       });
     }
     this.names.plants = ["wheat", "corn", 'carrot', 'soy', 'sugarCane', 'carrot', 'pumpkin', 'indigo', 'pepper', 'tomato', 'cotton', 'potato'];
-    this.names.items = ["bread", 'cream', 'popcorn', 'sweetPopcorn', 'syrup', 'whiteSugar', 
-    'chilePopcorn', 'brownSugar', 'butter', 'butterPopcorn', 'cheese', 'bread', 'chickenFeed', 
-    'potatoBread', 'blackberryCupcake', 'cookie', 'cornbread', 'pizza', 'raspberryCupcake', 
-    'spicyPizza', 'cowFeed', 'sheepFeed', 'pigFeed', 'sweater', 'cottonFabric', 'blueCap', 
-    'shirt', 'blueSweater', 'shorts', 'purpleDress']; //только крафты
+    this.names.items = ['tomatoJuice', 'brownSugar', 'pancakes', 'butterPopcorn',
+      'carrotJuice', 'blackberryCupcake', 'redScarf', 'omelet', 'applePie', 'cookie',
+      'pigFeed', 'sweetPopcorn', 'cherryJuice', 'creamCake', 'raspberryCupcake',
+      'blueCap', 'chickenFeed', 'berryJuice', 'hamburger', 'cherryPie', 'spicyPizza',
+      'carrotCake', 'appleJuice', 'cornbread', 'syrup', 'pizza', 'cheese', 'cream',
+      'whiteSugar', 'potatoBread', 'carrotPie', 'popcorn', 'chilePopcorn', 'purpleDress',
+      'blueSweater', 'bakedTomato', 'strawberryCake', 'pumpkinPie', 'cheesecake', 'bread',
+      'sweater', 'cottonFabric', 'sheepFeed', 'cowFeed', 'bakedPotato', 'shirt', 'shorts',
+      'baconPie', 'butter']; //только крафты
     this.names.animals = ['chicken', 'cow', 'pig', 'sheep'];
     this.names.obstacles = ["small_swamp"];
     this.mapImgNames = ["grass_1", "grass_2"];
